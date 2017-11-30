d9c0278110a6babc6f11003976585d33f01471bc
Pietra123 <33811404+Pietra123@users.noreply.github.com>
2017-11-30 12:23:53 +0100

    Update pag.py

--- pag.py
+++ pag.py
@@ -1,39 +1,35 @@
 import arcpy
+import networkx
 
-fc = "C:\Users\Wojciech\Desktop\semestr5\pag\pag projekt2\pag_projekt2\BDOT_Torun\L4_1_BDOT10k__OT_SKJZ_L.shp"
+# https://networkx.github.io/documentation/networkx-1.10/reference/index.html
+G = networkx.Graph()
 
-# Create a search cursor
-
-cursor = arcpy.da.SearchCursor(fc,["OID@", "SHAPE@"]) 
-
-# Create a list of string fields
+fc = "C:\Users\S276113.S403-K27\Desktop\pag_projekt2\BDOT_Torun\kuj_pom_miasto\L4_1_BDOT10k__OT_SKJZ_L.shp"
+cursor = arcpy.da.SearchCursor(fc,["OID@", "SHAPE@", "SHAPE@LENGTH"]) 
 
 points = []
-edges=[]
+edges = []
 i=0
 for row in cursor:
     for part in row[1]:
         if (part[0].X,part[0].Y) not in points:
+            G.add_node(i, X = part[0].X, Y = part[0].Y)
+            i = i + 1
             points.append((part[0].X,part[0].Y))
         if (part[-1].X,part[-1].Y) not in points:
+            G.add_node(i, X = part[-1].X, Y = part[-1].Y)
+            i = i + 1
             points.append((part[-1].X,part[-1].Y))
 
         id_from = points.index((part[0].X,part[0].Y))
         id_to = points.index((part[-1].X, part[-1].Y))
 
-        edges.append([i,part,id_from,id_to])
-
-        i=i+1
-        #print ("{0}, {1}, {2}".format(i,part[0].X,part[0].Y))
-        #print ("{0}, {1}, {2}".format(i,part[-1].X,part[-1].Y))
-    #break;
+        G.add_edge(id_from, id_to, ObjectID = row[0], length = row[2])
+        edges.append([id_from, id_to, row[0], row[2]])
+    break;
 print len(points)
+print networkx.number_of_nodes(G)
 print len(edges)
+print networkx.number_of_edges(G)
 
-vertexes = []
-i = 0
-
-for point in points:
-    vertexes.append([i,point[0],point[1]])
-    i = i+1
-print vertexes
+print networkx.get_node_attributes(G,['X','Y'])
